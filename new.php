9664708e2ac360b157b6e43698908e00ab5aa879
jasonwu513 <jasonwu513@gmail.com>
2019-05-29 00:01:49 +0800

    file operate

---
+++ new.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "123" ;
+echo __FILE__;
+//basename
+
+// mkdir('513');
+//rmdir('513');
+//rename('513.txt','514.txt');    can move file path
+//copy('a/513.txt','b/513.txt');
+
+
+?>
