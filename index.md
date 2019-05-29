e69de7cb8399008c86e9448fb9e19d87f2f7de99
jasonwu513 <jasonwu513@gmail.com>
2019-05-30 01:01:33 +0800

    include same name markdown file

---
+++ index.md
@@ -0,0 +1,30 @@
+# Index for Category
+
+## using function
+
+```php
+glob($path.type {})
+```
+> 搜尋路徑下的檔案&資料夾並放入array
+
+
+foreach($array as $v)
+> using i++ to do somthing at n round
+
+
+is_file(path);
+is_file(dir);
+
+count(array)
+計算 arrary 長度
+
+```php
+strops($v, "string")
+```
+是否含有字串
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,27 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        h1, h2, h3, h4, h5, h6, p, blockquote {
+   margin: 0;
+   padding: 0;
+}
+body {
+   font-family: "Helvetica Neue", Helvetica, "Hiragino Sans GB", Arial, sans-serif;
+   font-size: 13px;
+   line-height: 18px;
+   color: #737373;
+   background-color: white;
+   margin: 10px 13px 10px 13px;
+}
+table {
+   margin: 10px 0 15px 0;
+   border-collapse: collapse;
+}
+td,th {	
+   border: 1px solid #ddd;
+   padding: 3px 10px;
+}
+th {
+   padding: 5px 10px;	
+}
+
+a {
+   color: #0069d6;
+}
+a:hover {
+   color: #0050a3;
+   text-decoration: none;
+}
+a img {
+   border: none;
+}
+p {
+   margin-bottom: 9px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+   color: #404040;
+   line-height: 36px;
+}
+h1 {
+   margin-bottom: 18px;
+   font-size: 30px;
+}
+h2 {
+   font-size: 24px;
+}
+h3 {
+   font-size: 18px;
+}
+h4 {
+   font-size: 16px;
+}
+h5 {
+   font-size: 14px;
+}
+h6 {
+   font-size: 13px;
+}
+hr {
+   margin: 0 0 19px;
+   border: 0;
+   border-bottom: 1px solid #ccc;
+}
+blockquote {
+   padding: 13px 13px 21px 15px;
+   margin-bottom: 18px;
+   font-family:georgia,serif;
+   font-style: italic;
+}
+blockquote:before {
+   content:"\201C";
+   font-size:40px;
+   margin-left:-10px;
+   font-family:georgia,serif;
+   color:#eee;
+}
+blockquote p {
+   font-size: 14px;
+   font-weight: 300;
+   line-height: 18px;
+   margin-bottom: 0;
+   font-style: italic;
+}
+code, pre {
+   font-family: Monaco, Andale Mono, Courier New, monospace;
+}
+code {
+   background-color: #fee9cc;
+   color: rgba(0, 0, 0, 0.75);
+   padding: 1px 3px;
+   font-size: 12px;
+   -webkit-border-radius: 3px;
+   -moz-border-radius: 3px;
+   border-radius: 3px;
+}
+pre {
+   display: block;
+   padding: 14px;
+   margin: 0 0 18px;
+   line-height: 16px;
+   font-size: 11px;
+   border: 1px solid #d9d9d9;
+   white-space: pre-wrap;
+   word-wrap: break-word;
+}
+pre code {
+   background-color: #fff;
+   color:#737373;
+   font-size: 11px;
+   padding: 0;
+}
+sup {
+   font-size: 0.83em;
+   vertical-align: super;
+   line-height: 0;
+}
+* {
+   -webkit-print-color-adjust: exact;
+}
+@media screen and (min-width: 914px) {
+   body {
+      width: 854px;
+      margin:10px auto;
+   }
+}
+@media print {
+   body,code,pre code,h1,h2,h3,h4,h5,h6 {
+      color: black;
+   }
+   table, pre {
+      page-break-inside: avoid;
+   }
+}
+
+    </style>
+</head>
+<body>
+
+<h1>php 練習目錄</h1>
+    
 <?php
+
+include("./lib/Parsedown.php");
+
+
 
 $path    = './';
 $files = scandir($path);
 
 $dirArr = glob('./*');
-// 返回指定檔案類型
-// function globDir($dirname, $type=''){
-//    if($type ==''){
-//        $dirInfo = glob($dirname.'/*');
-//    }else{
-//        $dirInfo = glob($dirname.'/*.'.$type);
-//    }
-//    return $dirInfo;
-// }
 
-// $info = globDir('.','php');
-
-// echo "<pre>";
-// print_r($info);
-
-// 辨識所有目錄
-//$global $dirInfoLevel ;
-       // $dirInfoLevel = 0;
 function globDir($dirname, $type=''){
 
     
@@ -57,11 +195,7 @@ function globDir($dirname, $type=''){
             if (strpos($v, '.md') == false) {
                 echo  "<a href=\"" .$site. substr( $v , 2 )."\">".substr( $v , 2 )." </a> <br>";
             }
-            
-
-           
         }elseif(is_dir($v)){
-
             echo 'levelfoler: ' ." ". $dirInfoLevel. "<br> ";
             //  echo   "<a href=\"" .$site.$files[$i]."\">".$files[$i]." </a><br>";
             globDir($v);
@@ -76,21 +210,17 @@ function globDir($dirname, $type=''){
 
 globDir('.');
 
-
-echo '<h2>' . "php 練習目錄" . '</h2>';
-
-// echo $files;
-echo "<pre>";
-// print_r($files);
-// print_r($dirArr);
-// echo "<pre>";
-
-// for ($i=4; $i < count($files); $i++) { 
-//     $site = 'http://php.yaoshou365.com/';
-//     echo   "<a href=\"" .$site.$files[$i]."\">".$files[$i]." </a><br>";
-// }
+$basename = basename(__FILE__); 
+echo $basename;
+$Parsedown = new Parsedown();
+$markdownFile = fopen ( basename(__FILE__, '.php') . '.md'  , 'r');
+echo $Parsedown->text(fread($markdownFile,filesize('index.md')));
 
 
 
 
 ?>
+</body>
+</html>
+
+
