8ea640263e11e2c5b2f614440fd77572ae7b60d1
jasonwu513 <jasonwu513@gmail.com>
2019-06-05 22:39:02 +0800

    basic function form

---
+++ function/function.php
@@ -0,0 +1,14 @@
+<?php 
+    function sayHello($name){
+        echo "say hello to $name";
+        echo "<br>";
+    };
+
+    sayHello("Sally");
+
+    $name2 = "Billy";
+
+    sayHello($name2);
+    
+
+?>
