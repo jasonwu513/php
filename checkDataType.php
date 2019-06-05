ac58c6e8bb6c198cc44dc9741da01ea9ac4b476c
jasonwu513 <jasonwu513@gmail.com>
2019-06-05 22:37:32 +0800

    check datatype  print_r

--- checkDataType.php
+++ checkDataType.php
@@ -1,10 +1,17 @@
 <?php
     $x = 12.5;
+
     var_dump($x);
     echo "<br>";
+
     var_export($x);
     echo "<br>";
 
     $y = ["x","y","z"];
+    print_r($y);
+
+    echo "<br>";
+
+    $y = ["a" => "x","b" => "y","c" =>"z"];
     print_r($y);
 ?>
