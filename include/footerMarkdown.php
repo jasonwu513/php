b5d26009b25f54a1f954fecae3ed5c134e81b677
jasonwu513 <jasonwu513@gmail.com>
2019-07-25 05:45:57 +0800

    fix markdown read fail

--- include/footerMarkdown.php
+++ include/footerMarkdown.php
@@ -1,12 +1,22 @@
 <?php 
-
+// 引入 Parsedown
+include_once $_SERVER['DOCUMENT_ROOT'].'/lib/Parsedown.php';
 $Parsedown = new Parsedown();
+// echo basename(__FILE__, '.php');
+// echo $_SERVER['SCRIPT_FILENAME']."<br>";
 
-echo basename(__FILE__, '.php');
-// $markdownFile = fopen ( basename(__FILE__, '.php') . '.md'  , 'r');
-$markdownFile = fopen (basename($_SERVER['SCRIPT_FILENAME'] , '.php') . '.md'  , 'r');
-echo $Parsedown->text(fread($markdownFile,filesize(basename($_SERVER['SCRIPT_FILENAME'], '.php') . '.md')));
+//$_SERVER['SCRIPT_FILENAME'] 呼叫頁面路徑
+$fileNameMd = substr($_SERVER['SCRIPT_FILENAME'],0,-4).".md";
+// echo "mdpath : " . $fileNameMd;
 
+//確認同路徑下是否有同名但副檔名為md 的檔案
+if(file_exists ( $fileNameMd )){
+    // 存取markdown file 文件 
+    $markdownFile = fopen (basename($_SERVER['SCRIPT_FILENAME'] , '.php') . '.md'  , 'r');
+    // 印出 markdown file
+    echo $Parsedown->text(fread($markdownFile,filesize(basename($_SERVER['SCRIPT_FILENAME'], '.php') . '.md')));
+
+}    
 
 
 ?>
