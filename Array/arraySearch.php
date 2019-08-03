d59f89996a6dfff3b16b33a94d08b6f03630a67f
jasonwu513 <jasonwu513@gmail.com>
2019-08-03 09:30:29 +0800

    array functions

---
+++ Array/arraySearch.php
@@ -0,0 +1,7 @@
+<?php 
+
+$Arr = [1,5,25,125,625];
+echo array_search(max($Arr), $Arr);
+
+
+?>
---
+++ Array/createArray.php
@@ -0,0 +1,14 @@
+<?php 
+
+$Arr = [1,2,3,4,5];
+print_r($Arr);
+echo "<br>";
+$Arr1  = array('我', '是', '誰');
+print_r($Arr1);
+
+
+
+
+
+
+?>
